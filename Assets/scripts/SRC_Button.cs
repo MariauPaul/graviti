e5999077f26f07fae7530e1480130232197633fb
CASIMIRTPRO <casimirtpro@gmail.com>
2024-05-03 14:00:07 +0200

    SRC Button+Move

--- Assets/scripts/SRC_Button.cs
+++ Assets/scripts/SRC_Button.cs
@@ -1,10 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class SRC_Button : MonoBehaviour
 {
+    private bool rToMove = false;
     private Animator anim;
+    private GameObject iAmTheCase;
 
     void Start()
     {
@@ -14,10 +17,33 @@ public class SRC_Button : MonoBehaviour
     private void OnTriggerEnter(Collider other)
     {
         anim.SetBool("isTrigger", true);
+        if (other.tag == "Attrape")
+        {
+            iAmTheCase = other.GetComponent<GameObject>();
+            Placement();
+        }
     }
 
     private void OnTriggerExit(Collider other)
     {
-        anim.SetBool("isTrigger", false);
+        if (rToMove != true)
+        {
+            iAmTheCase = null;
+            anim.SetBool("isTrigger", false);
+        }
+    }
+
+    public void Placement()
+    {
+        if (iAmTheCase != null)
+        {
+            iAmTheCase.transform.parent = transform;
+            while (iAmTheCase.transform.position != Vector3.zero)
+            {
+                iAmTheCase.transform.position = Vector3.MoveTowards(iAmTheCase.transform.position, Vector3.zero, 0.01f);
+            }
+        }
+        rToMove = true;
+        iAmTheCase.GetComponent<Rigidbody>().isKinematic = true;
     }
 }
